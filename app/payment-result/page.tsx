f199c423ced17ffe37e70be4124a656aa7472333
amir <amirhosein.kiemasi@gmail.com>
2025-10-05 11:07:39 +0330

    fixe PaymentResultPage Page try4

--- app/payment-result/page.tsx
+++ app/payment-result/page.tsx
@@ -5,14 +5,15 @@ import PaymentResultContent from "../../components/PaymentResultContent";
 export default function PaymentResultPage() {
     return (
         <Container>
-            <Suspense fallback={
+            PaymentResultPage
+            {/* <Suspense fallback={
                 <div className="py-16 border-t border-divider shadow-lg rounded-2xl p-8 w-full max-w-md text-center">
                     <p>در حال بارگذاری...</p>
                 </div>
             }>
-                PaymentResultPage
-                {/* <PaymentResultContent /> */}
-            </Suspense>
+                
+                <PaymentResultContent />
+            </Suspense> */}
         </Container>
     );
 }
