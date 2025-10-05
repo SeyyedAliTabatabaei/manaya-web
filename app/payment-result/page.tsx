26edecb00fb0af04e02741cccf59ebaaf936b2f3
SeyyedAliTabatabaei <tabatabaeiali7@gmail.com>
2025-10-05 11:49:38 +0330

    fix payment result

--- app/payment-result/page.tsx
+++ app/payment-result/page.tsx
@@ -1,18 +1,17 @@
 import { Suspense } from "react";
 import Container from "@/components/Container";
 import PaymentResultContent from "../../components/PaymentResultContent";
- 
+
 export default function PaymentResultPage() {
     return (
         <Container>
-            PaymentResultPage
             <Suspense fallback={
                 <div className="py-16 border-t border-divider shadow-lg rounded-2xl p-8 w-full max-w-md text-center">
                     <p>در حال بارگذاری...</p>
                 </div>
             }>
                 
-                <PaymentResultContent />
+                <PaymentResultContent/>
             </Suspense>
         </Container>
     );
--- components/PaymentResultContent.tsx
+++ components/PaymentResultContent.tsx
@@ -1,42 +1,49 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import Container from "@/components/Container";
 
 export default function PaymentResultContent() {
     const searchParams = useSearchParams();
 
     const success = searchParams.get("success") === "true";
     const message = searchParams.get("message") || "";
-    const amount = searchParams.get("data[amount]");
-    const refId = searchParams.get("data[ref_id]");
-    const daysRemaining = searchParams.get("data[days_remaining]");
+    const amount = searchParams.get("amount");
+    const refId = searchParams.get("ref_id");
+    const daysRemaining = searchParams.get("days_remaining");
 
     return (
-        <div className="mx-auto   border-divider shadow-lg rounded-2xl p-8 w-full max-w-md text-center">
-            <h1
-                className={`text-2xl font-bold mb-4 ${
-                    success ? "text-green-600" : "text-red-600"
-                }`}
-            >
-                {success ? "✅ پرداخت موفق" : "❌ پرداخت ناموفق"}
-            </h1>
+        <Container>
+            <div className="py-16 bg-surface border-t border-divider shadow-lg rounded-2xl p-8 w-full max-w-md text-center">
+                <h1
+                    className={`text-2xl font-bold mb-4 ${
+                        success ? "text-green-600" : "text-red-600"
+                    }`}
+                >
+                    {success ? "✅ پرداخت موفق" : "❌ پرداخت ناموفق"}
+                </h1>
 
-            <p className="text-text-secondary mb-6">{decodeURIComponent(message)}</p>
+                <p className="text-text-secondary mb-6">{decodeURIComponent(message)}</p>
 
-            {success && (
-                <div className="space-y-2 text-text-primary">
-                    <p><span className="text-lg">مبلغ: </span>{amount} ریال</p>
-                    <p><span className="text-lg">کد پیگیری: </span>{refId}</p>
-                    <p><span className="text-lg">روزهای باقی‌مانده اشتراک: </span>{daysRemaining}</p>
-                </div>
-            )}
+                {success && (
+                    <div className="space-y-2 text-text-primary">
+                        <p><span className="text-lg">مبلغ: </span>{amount} ریال</p>
+                        <p><span className="text-lg">کد پیگیری: </span>{refId}</p>
+                        <p><span className="text-lg">روزهای باقی‌مانده اشتراک: </span>{daysRemaining}</p>
+                    </div>
+                )}
 
-            {/*<button*/}
-            {/*    onClick={() => (window.location.href = "/")}*/}
-            {/*    className="mt-6 w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-lg transition"*/}
-            {/*>*/}
-            {/*    بازگشت به خانه*/}
-            {/*</button>*/}
-        </div>
+                <button
+                    onClick={() => {
+                        window.location.href = "manaya://payment/result";
+                    }}
+                    className="mt-6 w-full bg-primary hover:bg-blue-700 text-white py-2 rounded-lg transition"
+                >
+                    بازگشت به مانایا
+                </button>
+            </div>
+        </Container>
     );
 }
+
+
